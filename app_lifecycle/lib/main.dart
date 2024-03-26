08cb066056e62680457667dfc1efefd8ac13c517
“Helly” <luungoclan125690@gmail.com>
2024-03-26 14:21:07 +0700

    feat(app-lifecycle): app lifecycle demo

---
+++ app_lifecycle/lib/main.dart
@@ -0,0 +1,188 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo App Lifecycle',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  /// 
+  late final AppLifecycleListener _listener;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    _listener = AppLifecycleListener(
+      onDetach: _onDetach,
+      onHide: _onHide,
+      onInactive: _onInactive,
+      onPause: _onPause,
+      onRestart: _onRestart,
+      onResume: _onResume,
+      onShow: _onShow,
+      onStateChange: _onStateChanged,
+      onExitRequested: _onExitRequested,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _listener.dispose();
+
+    super.dispose();
+  }
+
+  void _onDetach() => debugPrint('onDetach');
+
+  void _onHide() => debugPrint('onHide');
+
+  void _onInactive() => debugPrint('onInactive');
+
+  void _onPause() => debugPrint('onPause');
+
+  void _onRestart() => debugPrint('onRestart');
+
+  void _onResume() => debugPrint('onResume');
+
+  void _onShow() => debugPrint('onShow');
+
+  void _onStateChanged(AppLifecycleState state) {
+    // Track state changes
+  }
+
+  // Ask the user if they want to exit the app. If the user
+  // cancels the exit, return AppExitResponse.cancel. Otherwise,
+  // return AppExitResponse.exit.
+  Future<AppExitResponse> _onExitRequested() async {
+    final response = await showDialog<AppExitResponse>(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog.adaptive(
+        title: const Text('Are you sure you want to quit this app?'),
+        content: const Text('All unsaved progress will be lost.'),
+        actions: [
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: () {
+              Navigator.of(context).pop(AppExitResponse.cancel);
+            },
+          ),
+          TextButton(
+            child: const Text('Ok'),
+            onPressed: () {
+              Navigator.of(context).pop(AppExitResponse.exit);
+            },
+          ),
+        ],
+      ),
+    );
+
+    return response ?? AppExitResponse.exit;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(40.0),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Enter your name',
+                  alignLabelWithHint: true,
+                  floatingLabelAlignment: FloatingLabelAlignment.center,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const NewPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Go to new page'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class NewPage extends StatelessWidget {
+  const NewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Page'),
+      ),
+      body: const Center(
+        child: Text('This is a new page'),
+      ),
+    );
+  }
+}
